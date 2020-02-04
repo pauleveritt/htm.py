06d632be2c360ed5558cbb8a9da27778298861b3
Paul Everitt <pauleveritt@me.com>
2020-02-04 12:33:18 -0500

    Re-organize before doing some variations.

--- tests/test_injector.py
+++ tests/test_injector.py
@@ -1,18 +1,25 @@
 import unittest
-from htm.injected_html import html
+from htm.passthrough import html
 
-
-# noinspection PyPep8Naming
-def Heading(header='Some Default', children=(), config=None):
-    label = config['heading_label']
-    return html('<header>{label}: Hello {header}</header>')
+expected = '<header>Some Label: Hello My Components</header>'
 
 
 class TestInjector(unittest.TestCase):
-    def test_single_arg(self):
+    def test_passthrough(self):
+        """
+Simplest possible case: a value passed into the ``html`` function
+is passed all the way along to a component.
+
+        """
+        # noinspection PyPep8Naming
+        def Heading(header='Some Default', children=(), config=None):
+            label = config['heading_label']
+            return html('<header>{label}: Hello {header}</header>')
+
         config = dict(heading_label='Some Label')
-        expected = '<header>Some Label: Hello My Components</header>'
+
         actual = str(html("""
     <{Heading} header='My Components'><//>
 """, config=config))
+
         self.assertEqual(expected, actual)
